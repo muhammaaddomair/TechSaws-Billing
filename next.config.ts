3074dfb67efbb3a4519ff54a90ccfed9933ad269
Muhammad Domair <muhammaaddomair@gmail.com>
2026-04-23 10:40:31 +0500

    include prisma engines in next output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typedRoutes: true
+  typedRoutes: true,
+  outputFileTracingIncludes: {
+    "/*": ["./generated/prisma/**/*", "./node_modules/.prisma/client/**/*"],
+    "/**/*": ["./generated/prisma/**/*", "./node_modules/.prisma/client/**/*"]
+  }
 };
 
 export default nextConfig;
